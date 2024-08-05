81176305652a6d9973f82fbfc99cf4eaaf64a6e3
IfNoise <black_russan@mail.ru>
2024-08-05 23:29:16 +0700

    изменено:      app.js

--- app.js
+++ app.js
@@ -22,18 +22,18 @@ timerManager.init();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json({ extended: true }));
-app.use("light",express.static(path.join(__dirname, "client",'dist')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/light', express.static(path.join(__dirname, 'client', 'dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  })
+}
 
 app.use("/light/api/devices", require("./routes/devices.route"));
 app.use("/light/api/timers", require("./routes/timers.route"));
 app.use("/light/api/lightChannels", require("./routes/lightChannels.route"));
-
-
-
-//
-  
-
-
 
 app.listen(port, () => {
   console.log(`Server app listening at http://localhost:${port}`);
