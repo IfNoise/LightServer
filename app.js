bad296b84504edb9726e546c09f01926f0d5af69
IfNoise <black_russan@mail.ru>
2024-08-05 23:33:14 +0700

    изменено:      app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json({ extended: true }));
 if (process.env.NODE_ENV === 'production') {
   app.use('/light', express.static(path.join(__dirname, 'client', 'dist')))
 
-  app.get('*', (req, res) => {
+  app.get('/light', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   })
 }
