c10158dc007b49a0712e98ed5d80e1c02e1252b3
IfNoise <black_russan@mail.ru>
2024-08-06 14:26:47 +0700

    изменено:      app.js

--- app.js
+++ app.js
@@ -34,9 +34,6 @@ app.use("/api/lightChannels", require("./routes/lightChannels.route"));
 
   app.use('/', express.static(path.join(__dirname, 'client', 'dist')))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-  })
 
 app.listen(port, () => {
   console.log(`Server app listening at http://localhost:${port}`);
