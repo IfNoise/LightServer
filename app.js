37586d51065b11e9cfa023616bf7573266786a38
IfNoise <black_russan@mail.ru>
2024-08-06 13:59:16 +0700

    изменено:      app.js

--- app.js
+++ app.js
@@ -31,13 +31,13 @@ app.use("/api/devices", require("./routes/devices.route"));
 app.use("/api/timers", require("./routes/timers.route"));
 app.use("/api/lightChannels", require("./routes/lightChannels.route"));
 
-if (process.env.NODE_ENV === 'production') {
+
   app.use('/', express.static(path.join(__dirname, 'client', 'dist')))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   })
-}
+
 app.listen(port, () => {
   console.log(`Server app listening at http://localhost:${port}`);
 })
